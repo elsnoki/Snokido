329e71f9e86d828901335e82bec6a7ce636dfea0
elsnoki <livaimark@gmail.com>
2026-03-09 23:52:16 +0100

    Update header.js

--- header.js
+++ header.js
@@ -51,8 +51,10 @@ const headerHTML = `
       <a class="tab tab-role" href="role.html">Rôles</a>
       <a class="tab tab-humble" href="humble.html">Hommes Humbles</a>
       <a class="tab tab-gen" href="gen.html">Générations</a>
-      <a class="tab tab-pantheon" href="pantheon.html">Panthéon</a>
+      <a class="tab tab-pantheon" href="pantheon.html">Panthéon des Légendes</a>
       <a class="tab tab-pantheon2" href="pantheon2.html">Panthéon de la honte</a>
+      <a class="tab tab-noob" href="pantheon3.html">Panthéon des Noobs</a>
+      
 
     </nav>
   </div>
@@ -109,6 +111,7 @@ document.addEventListener("keydown", (e) => {
   if(file === "pause.html" && linkUnique) linkUnique.classList.add("is-active");
   if((file === "pauses.html" || file === "pauses_total.html") && linkTotal) linkTotal.classList.add("is-active");
 })();
+
 
 
 
