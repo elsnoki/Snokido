4c13fa2c504c15ea6e1d9db520d1b0168c74a3d5
elsnoki <livaimark@gmail.com>
2026-03-23 21:15:40 +0100

    Update success.js

--- success.js
+++ success.js
@@ -1,6 +1,6 @@
 window.SUCCESS = {
 
-talk:[
+talktakashi:[
 "Exp Noté","Exp Joué","Knight","King","Monster","Le Divin",
 "Blood Note","Élite","Les Élus","Crystal Rank",
 "Snoki Collector","Kara Collector","Multi·Kara",
@@ -9,7 +9,7 @@ talk:[
 "Hight","Épic Titan","Imperator","Champion Désertique"
 ],
 
-alex:[
+alexc2109:[
 "Exp Noté","Exp Joué","Knight","King","Monster","Le Divin",
 "Blood Note","Élite","Les Élus","Crystal Rank",
 "Kara Collector","Multi·Kara",
@@ -23,7 +23,7 @@ francoeur:[
 "Kara Collector","Collectionneur d'Artefacts"
 ],
 
-gladiator:[
+thegladiator:[
 "Exp Joué","Knight","King","Monster",
 "Élite","Les Élus"
 ],
@@ -32,7 +32,137 @@ abelard:[
 "Champion Désertique","Imperator","Épic Titan",
 "Tempest Nocturne","Collectionneur d'Artefacts",
 "Kara Collector","Les Élus","Élite","Exp Joué"
+],
+
+jordanio:[
+"Tempest Nocturne","Collectionneur d'Artefacts",
+"Kara Collector","Élite","Exp Joué","Exp Noté"
+],
+
+hoopa:[
+"Épic Titan","Kara Collector","Snoki Collector",
+"Élite","Exp Joué","Exp Noté"
+],
+
+josue455:[
+"Exp Noté","Exp Joué","Élite","Kara Collector",
+"Hight","Épic Titan","Imperator"
+],
+
+alexis279:[
+"Hight","Tempest Nocturne","Collectionneur d'Artefacts",
+"Kara Collector","Élite","Exp Joué","Exp Noté"
+],
+
+lumia:[
+"Roi Suprême","Kara Collector","Élite","Exp Joué"
+],
+
+ko0pa:[
+"Homme humble","Tempest Nocturne","Roi Suprême",
+"Collectionneur d'Artefacts","Kara Collector",
+"Exp Noté","Exp Joué"
+],
+
+cirpacha:[
+"Exp Noté","Exp Joué","Kara Collector"
+],
+
+esdras950:[
+"Hight","Kara Collector","Exp Joué","Exp Noté"
+],
+
+unit:[
+"Exp Joué","Kara Collector"
+],
+
+golder:[
+"Homme humble","Épic Titan","Kara Collector","Exp Joué"
+],
+
+largo672:[
+"Exp Noté","Exp Joué","Kara Collector","Épic Titan","Imperator"
+],
+
+djabrail:[
+"Exp Noté","Exp Joué","Kara Collector","Épic Titan"
+],
+
+houka:[
+"Exp Joué","Kara Collector","Tempest Nocturne","Collectionneur d'Artefacts"
+],
+
+justice:[
+"Exp Joué","Kara Collector","Tempest Nocturne","Collectionneur d'Artefacts"
+],
+
+tellor:[
+"Exp Noté","Exp Joué","Kara Collector"
+],
+
+pingu:[
+"Kara Collector","Homme humble","Exp Joué"
+],
+
+redbot:[
+"Homme humble","Tempest Nocturne","Collectionneur d'Artefacts",
+"Kara Collector","Exp Joué"
+],
+
+venom_:[
+"Tempest Nocturne","Collectionneur d'Artefacts",
+"Kara Collector","Exp Joué"
+],
+
+blackout:[
+"Kara Collector","Exp Joué"
+],
+
+marcosino:[
+"Exp Joué"
+],
+
+largojunior:[
+"Kara Collector","Blood Note","Exp Joué","Exp Noté"
+],
+
+seka:[
+"Exp Joué"
+],
+
+cahincaha:[
+"Exp Joué","Kara Collector"
+],
+
+athena:[
+"Kara Collector"
+],
+
+uyscuty:[
+"Exp Joué","Kara Collector"
+],
+
+betelgeuse:[
+"Exp Joué","Kara Collector","Homme humble"
+],
+
+aldebaran:[
+"Exp Joué","Kara Collector","Homme humble"
+],
+
+acno:[
+"Kara Collector"
+],
+
+napoleon1er:[],
+
+snokido:[
+"Kara Collector","Exp Joué","Tempest Nocturne",
+"Roi Suprême","Collectionneur d'Artefacts","Épic Titan"
+],
+
+snokido2:[
+"Kara Collector","Exp Joué","Imperator","Épic Titan"
 ]
 
-// 👉 continue avec tous les autres
 };
