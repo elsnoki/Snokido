5ac1e2244ba9a1727e19e0a5249903af30e91bfd
elsnoki <livaimark@gmail.com>
2026-03-23 14:35:08 +0100

    Create sucess.js

---
+++ sucess.js
@@ -0,0 +1,38 @@
+window.SUCCESS = {
+
+talk:[
+"Exp Noté","Exp Joué","Knight","King","Monster","Le Divin",
+"Blood Note","Élite","Les Élus","Crystal Rank",
+"Snoki Collector","Kara Collector","Multi·Kara",
+"Collectionneur d'Artefacts",
+"Roi Suprême","Tempest Nocturne",
+"Hight","Épic Titan","Imperator","Champion Désertique"
+],
+
+alex:[
+"Exp Noté","Exp Joué","Knight","King","Monster","Le Divin",
+"Blood Note","Élite","Les Élus","Crystal Rank",
+"Kara Collector","Multi·Kara",
+"Collectionneur d'Artefacts",
+"Roi Suprême","Tempest Nocturne"
+],
+
+francoeur:[
+"Exp Joué","Knight","King","Monster",
+"Élite","Les Élus",
+"Kara Collector","Collectionneur d'Artefacts"
+],
+
+gladiator:[
+"Exp Joué","Knight","King","Monster",
+"Élite","Les Élus"
+],
+
+abelard:[
+"Champion Désertique","Imperator","Épic Titan",
+"Tempest Nocturne","Collectionneur d'Artefacts",
+"Kara Collector","Les Élus","Élite","Exp Joué"
+]
+
+// 👉 continue avec tous les autres
+};
