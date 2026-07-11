c917d501fb2abe1b4c4d24cd44638e7b3d962e31
elsnoki <livaimark@gmail.com>
2026-07-12 01:07:25 +0200

    Update data.js

--- data.js
+++ data.js
@@ -432,7 +432,7 @@ const joueurs = [
   nom:"Clovis1er", avatar:"avatar/36.jpg", gen:7, inscription:"14-02-2026",
   modes:{
     nv100:{
-      jours:null, atteint_le:"—", rang:"—", xp:0,
+      jours:148, atteint_le:"12-07-2026", rang:"—", xp:0,
       pauses_total:{ nb:0, mois:0, jours:0 },
       pauses_detail:[]
     }
